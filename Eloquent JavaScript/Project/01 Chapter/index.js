f06d1edd78a34470c2f2355c023a131b1ecaee7e
wasim <wasimakhter2030@gmail.com>
2024-04-08 16:58:28 +0530

    Chapter 1: values, numbers, arithmetic, specical no, strings, unary operator, boolean values, comparisition, Logical opetators, empty values, automatic type conversition

--- Eloquent JavaScript/Project/01 Chapter/index.js
+++ Eloquent JavaScript/Project/01 Chapter/index.js
@@ -1,19 +1,54 @@
 // numbers;
-
-// console.log(Number("11"));
+console.log(Number("11"));
 // console.log(00001101);
 // console.log(Number(00001101));
 
 // strings
-
 console.log(`its
 a
 backtick
 `);
-
 console.log("hello \nworld");
 console.log("\thello world");
-
 console.log('A newline character is written like "\\n".');
-
 console.log(`Half of 100 is: ${100 / 2}`);
+
+// unary operator
+console.log(typeof 7.9);
+console.log(typeof "wasim");
+
+// binary opetator
+console.log(4 + 9);
+
+// boolean type
+// producing boolen by using comparisition
+console.log(9 > 4);
+console.log(9 < 4);
+
+// comparision operator: < and > => binary operator
+console.log("a" > "Z"); //uppercase letter < lowercase letter
+
+console.log(NaN == NaN);
+console.log("***");
+console.log(1 + 1 == 2 && 10 * 10 > 50);
+
+// ternary opetator - operates on three values
+console.log(true ? 1 : 2);
+console.log(false ? 1 : 2);
+
+// automatic type conversion
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log("dgd" * 2);
+console.log(false == 0);
+console.log(NaN == NaN);
+console.log("**************");
+console.log(null == undefined);
+console.log(null == 0);
+
+console.log("**************");
+console.log(false == 0);
+console.log(false == NaN); // ???????
+console.log(false == "");
